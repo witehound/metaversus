76d80614fb4bb85032d3e85546d6e550e786a79b
witehound <netvid00@gmail.com>
2023-01-16 13:23:29 +0100

    finished explore component

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -8,27 +8,44 @@ import { staggerContainer } from "../utils/motion";
 import { TypingText, ExploreCard, TitleText } from "../components";
 import { exploreWorlds } from "../constants";
 
-const Explore = () => (
-  <section className={`${styles.paddings}`} id="explore">
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView={"show"}
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title={"| The World"} textStyles="text-center" />
-      <TitleText
-        title={
-          <>
-            Choose The World You Want <br className="md:block hidden" /> To
-            Explore
-          </>
-        }
-        textStyles="text-center"
-      />
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState("world-2");
+
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title={"| The World"} textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose The World You Want <br className="md:block hidden" /> To
+              Explore
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div
+          className={`mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5`}
+        >
+          {exploreWorlds.map((el, i) => (
+            <ExploreCard
+              key={el.id}
+              {...el}
+              index={i}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
