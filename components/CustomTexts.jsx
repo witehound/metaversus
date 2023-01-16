9ce04a1ebe901c5021c74b7918d01a4528be6b3e
witehound <netvid00@gmail.com>
2023-01-16 12:13:27 +0100

    initial commit and finished typingtext component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,20 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+//internal import
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((el, i) => (
+      <motion.span variants={textVariant2} key={i}>
+        {el === " " ? "\u00A0" : el}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,24 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+//internal imports
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+import { TypingText } from "../components";
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10 `}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title={"| About Metaversus"} textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
