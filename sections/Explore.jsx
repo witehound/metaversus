348beea03b9a072c5c5ba4f5b3f0560d3b64d7e2
witehound <netvid00@gmail.com>
2023-01-16 12:48:09 +0100

    finished titleText component

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,33 @@
-'use client';
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+//internal imports
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+import { exploreWorlds } from "../constants";
 
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title={"| The World"} textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Choose The World You Want <br className="md:block hidden" /> To
+            Explore
+          </>
+        }
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
 );
 
